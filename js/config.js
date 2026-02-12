6088faf27f7f6afda091858f69fbd9d12dfbf444
LaxmanSidhu <laxman2025.sawant@gmail.com>
2026-02-12 18:33:59 +0530

    thumbnails

--- js/config.js
+++ js/config.js
@@ -9,11 +9,11 @@ const BASE_URL =
 
 const SHEETS = {
   hollywood: { name: "Hollywood", gid: "0", thumbnail: "https://i.cdn.newsbytesapp.com/images/l70420220408162006.jpeg" },
-  bollywood: { name: "Bollywood", gid: "709264029", thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7hobRSSZ_MmdDjoSuxTeb3Il_QZWGyEfYpg&s" },
-  tollywood: { name: "Tollywood", gid: "1128818627", thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh9sIm89ahznnWN6sUMdSvYpgX8yKb2X4tcg&s" },
-  korean: { name: "Korean", gid: "891613117", thumbnail: "https://cdn.kbizoom.com/media/2025/05/11083027/attophimxhmqn073dme31-1746876333551-1746876333725314482825webp-1105258.webp" },
-  marathi: { name: "Marathi", gid: "120892353", thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTD3j3Jcjuglhwy1bu_rXYgi7ySTDLkRnr6cA&s" },
-  fav_movie: { name: "Favourite Movies", gid: "894280801", thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlCIxHxGeiySeRFasi70I-4Ja0l_gTFeuJwA&s" },
-  watchlist: { name: "Watchlist", gid: "549028903", thumbnail: "https://static.toiimg.com/thumb/msid-125803323,imgsize-26552,width-400,resizemode-4/dhurandhar-ending-explained.jpg" },
-  web_series: { name: "Web Series", gid: "1808896095", thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh9sIm89ahznnWN6sUMdSvYpgX8yKb2X4tcg&s" }
+  bollywood: { name: "Bollywood", gid: "709264029", thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt2fmK1VUI7NVq_47_cl8bBovZ8Lg8LXlAYQ&s" },
+  tollywood: { name: "Tollywood", gid: "1128818627", thumbnail: "https://www.newsbharati.com/Encyc/2022/8/25/South-Indian-films_1806ffc02e4_large_202208251904110849_H@@IGHT_400_W@@IDTH_750.jpg" },
+  korean: { name: "Korean", gid: "891613117", thumbnail: "https://media.timeout.com/images/106247272/750/562/image.jpg" },
+  marathi: { name: "Marathi", gid: "120892353", thumbnail: "https://staticprintenglish.theprint.in/wp-content/uploads/2024/07/marathi_movie_industry_elitism.jpg" },
+  fav_movie: { name: "Favourite Movies", gid: "894280801", thumbnail: "https://contentful.harrypotter.com/usf1vwtuqyxm/4Lx4yGQ3W0daVknu7ayizS/d386272ff997df7578c175fb8b912ef8/HP-F1-philosophers-stone-harry-ron-hermione-hogwarts-express-web-landscape?q=75&fm=jpg&w=2560" },
+  watchlist: { name: "Watchlist", gid: "549028903", thumbnail: "https://www.slashfilm.com/img/gallery/12-strongest-avengers-in-the-mcu-ranked/intro-1746204078.jpg" },
+  web_series: { name: "Web Series", gid: "1808896095", thumbnail: "https://images.saymedia-content.com/.image/t_share/MjA2NDEzODM1MDAxNjY4OTQ4/22-greatest-premium-channel-tv-series.jpg" }
 };
